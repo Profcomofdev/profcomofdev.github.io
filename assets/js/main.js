f49fe4c0005db572384b80419c01841b8e279dca
root <root@localhost.localdomain>
2022-10-15 05:56:30 -0400

    Updated landing page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,5 @@
-const eye = document.querySelector('.pc-svg');  
-  window.addEventListener('mousemove', (event) => {  
-  const x = (window.innerWidth / 2 - event.pageX) / 10;  
-  const y = (window.innerHeight / 2 - event.pageY) / 10;
-  let root_svg = document.querySelector('.pc-svg');
-  let all_svg_roots = root_svg.querySelectorAll('.pc-anim');
-  for (let svg in all_svg_roots){
-     let ss = all_svg_roots[svg];
-     try{
-        ss.style = `transform: translateY(-${(2)*(y)}px) translateX(${(-1)*(x)}px);`; 
-     } catch{
-        let u = '';
-     }
-  }
-  }); 
+document.querySelector('.main').addEventListener("mousemove", (e) => {
+   let main = document.querySelector('.main');
+   main.style.backgroundPositionX = (-e.offsetX)/100 + "px";
+   main.style.backgroundPositionY = (-e.offsetY)/100 + "px";
+});
